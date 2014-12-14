6ec0f3d792c632ede0e56d3a020b50ae553a23fd
Tomasz Duda <tomek@ddart.pl>
2014-12-14 16:54:54 +0100

    Improved the Argument

--- src/Argument.php
+++ src/Argument.php
@@ -46,9 +46,113 @@ namespace PowerEcommerce\System {
         /**
          * @return bool
          */
+        function isArray()
+        {
+            return is_array($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isBool()
+        {
+            return is_bool($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isCallable()
+        {
+            return is_callable($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isDouble()
+        {
+            return is_double($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isFloat()
+        {
+            return is_float($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isInt()
+        {
+            return is_int($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isInteger()
+        {
+            return is_integer($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isLong()
+        {
+            return is_long($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
         function isNull()
         {
             return (null === $this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isNumeric()
+        {
+            return is_numeric($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isObject()
+        {
+            return is_object($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isReal()
+        {
+            return is_real($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isResource()
+        {
+            return is_resource($this->_value);
+        }
+
+        /**
+         * @return bool
+         */
+        function isScalar()
+        {
+            return is_scalar($this->_value);
         }
 
         /**
@@ -65,6 +169,31 @@ namespace PowerEcommerce\System {
         function isNumber()
         {
             return is_numeric($this->_value);
+        }
+
+        /**
+         * @param int $flags
+         * @return bool
+         */
+        function is($flags)
+        {
+            if (($flags & TypeCode::PHP_ARRAY) && $this->isArray()) return true;
+            if (($flags & TypeCode::PHP_BOOL) && $this->isBool()) return true;
+            if (($flags & TypeCode::PHP_CALLABLE) && $this->isCallable()) return true;
+            if (($flags & TypeCode::PHP_DOUBLE) && $this->isDouble()) return true;
+            if (($flags & TypeCode::PHP_FLOAT) && $this->isFloat()) return true;
+            if (($flags & TypeCode::PHP_INT) && $this->isInt()) return true;
+            if (($flags & TypeCode::PHP_INTEGER) && $this->isInteger()) return true;
+            if (($flags & TypeCode::PHP_LONG) && $this->isLong()) return true;
+            if (($flags & TypeCode::PHP_NULL) && $this->isNull()) return true;
+            if (($flags & TypeCode::PHP_NUMERIC) && $this->isNumeric()) return true;
+            if (($flags & TypeCode::PHP_OBJECT) && $this->isObject()) return true;
+            if (($flags & TypeCode::PHP_REAL) && $this->isReal()) return true;
+            if (($flags & TypeCode::PHP_RESOURCE) && $this->isResource()) return true;
+            if (($flags & TypeCode::PHP_SCALAR) && $this->isScalar()) return true;
+            if (($flags & TypeCode::PHP_STRING) && $this->isString()) return true;
+
+            return false;
         }
 
         /**
@@ -121,6 +250,23 @@ namespace PowerEcommerce\System {
         function ofTimeZone()
         {
             return $this->_value instanceof TimeZone;
+        }
+
+        /**
+         * @param int $flags
+         * @return bool
+         */
+        function of($flags)
+        {
+            if (($flags & TypeCode::BLANK) && $this->ofBlank()) return true;
+            if (($flags & TypeCode::CONTAINER) && $this->ofContainer()) return true;
+            if (($flags & TypeCode::DATETIME) && $this->ofDateTime()) return true;
+            if (($flags & TypeCode::NUMBER) && $this->ofNumber()) return true;
+            if (($flags & TypeCode::OBJECT) && $this->ofObject()) return true;
+            if (($flags & TypeCode::STRING) && $this->ofString()) return true;
+            if (($flags & TypeCode::TIMEZONE) && $this->ofTimeZone()) return true;
+
+            return false;
         }
 
         /**
---
+++ tests/ArgumentTest.php
@@ -0,0 +1,268 @@
+<?php
+
+/**
+ * Copyright (c) 2015 DD Art Tomasz Duda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace PowerEcommerce\System {
+
+    /**
+     * Class ArgumentTest
+     * @package PowerEcommerce\System
+     */
+    class ArgumentTest extends \PHPUnit_Framework_TestCase
+    {
+        function testIsArray()
+        {
+            $this->assertTrue((new Argument(['ok']))->isArray());
+            $this->assertFalse((new Argument('no'))->isArray());
+        }
+
+        function testIsBool()
+        {
+            $this->assertTrue((new Argument(true))->isBool());
+            $this->assertFalse((new Argument(1))->isBool());
+        }
+
+        function testIsCallable()
+        {
+            $this->assertTrue((new Argument(function () {
+            }))->isCallable());
+            $this->assertTrue((new Argument([$this, 'testIsCallable']))->isCallable());
+            $this->assertTrue((new Argument('\PowerEcommerce\System\ArgumentTest::testIsCallable'))->isCallable());
+            $this->assertFalse((new Argument(1))->isCallable());
+        }
+
+        function testIsDouble()
+        {
+            $this->assertTrue((new Argument(1.0))->isDouble());
+            $this->assertFalse((new Argument(1))->isDouble());
+        }
+
+        function testIsFloat()
+        {
+            $this->assertTrue((new Argument(1.0))->isFloat());
+            $this->assertFalse((new Argument(1))->isFloat());
+        }
+
+        function testIsInt()
+        {
+            $this->assertTrue((new Argument(1))->isInt());
+            $this->assertFalse((new Argument(1.0))->isInt());
+        }
+
+        function testIsInteger()
+        {
+            $this->assertTrue((new Argument(1))->isInteger());
+            $this->assertFalse((new Argument(1.0))->isInteger());
+        }
+
+        function testIsLong()
+        {
+            $this->assertTrue((new Argument(1))->isInteger());
+            $this->assertFalse((new Argument(1.0))->isInteger());
+        }
+
+        function testIsNull()
+        {
+            $this->assertTrue((new Argument(null))->isNull());
+            $this->assertFalse((new Argument(0))->isNull());
+        }
+
+        function testIsNumeric()
+        {
+            $this->assertTrue((new Argument('123.56'))->isNumeric());
+            $this->assertTrue((new Argument(123.56))->isNumeric());
+            $this->assertTrue((new Argument(123))->isNumeric());
+            $this->assertFalse((new Argument('123,12'))->isNumeric());
+        }
+
+        function testIsNumber()
+        {
+            $this->assertTrue((new Argument('123.56'))->isNumber());
+            $this->assertTrue((new Argument(123.56))->isNumber());
+            $this->assertTrue((new Argument(123))->isNumber());
+            $this->assertFalse((new Argument('123,12'))->isNumber());
+        }
+
+        function testIsObject()
+        {
+            $this->assertTrue((new Argument(new \StdClass()))->isObject());
+            $this->assertFalse((new Argument(null))->isObject());
+        }
+
+        function testIsReal()
+        {
+            $this->assertTrue((new Argument(1.2))->isReal());
+            $this->assertFalse((new Argument(1))->isReal());
+        }
+
+        function testIsResource()
+        {
+            $this->assertTrue((new Argument(tmpfile()))->isResource());
+            $this->assertFalse((new Argument(null))->isResource());
+        }
+
+        function testIsScalar()
+        {
+            $this->assertTrue((new Argument(1))->isScalar());
+            $this->assertTrue((new Argument(1.2))->isScalar());
+            $this->assertTrue((new Argument('1.2'))->isScalar());
+            $this->assertTrue((new Argument(true))->isScalar());
+
+            $this->assertFalse((new Argument(null))->isScalar());
+            $this->assertFalse((new Argument([]))->isScalar());
+            $this->assertFalse((new Argument(new \StdClass()))->isScalar());
+            $this->assertFalse((new Argument(tmpfile()))->isScalar());
+        }
+
+        function testIsString()
+        {
+            $this->assertTrue((new Argument('1.2'))->isString());
+
+            $this->assertFalse((new Argument(null))->isString());
+            $this->assertFalse((new Argument([]))->isString());
+            $this->assertFalse((new Argument(new \StdClass()))->isString());
+            $this->assertFalse((new Argument(tmpfile()))->isString());
+            $this->assertFalse((new Argument(1))->isString());
+            $this->assertFalse((new Argument(1.2))->isString());
+        }
+
+        function testIs()
+        {
+            $this->assertTrue((new Argument(['ok']))->is(TypeCode::PHP_ARRAY));
+            $this->assertTrue((new Argument('no'))->is(TypeCode::PHP_ARRAY | TypeCode::STRING | TypeCode::PHP_STRING));
+            $this->assertTrue((new Argument('no'))->is(TypeCode::PHP_ARRAY | TypeCode::STRING | ~TypeCode::PHP_STRING));
+            $this->assertFalse((new Argument('no'))->is(TypeCode::PHP_ARRAY | TypeCode::STRING));
+            $this->assertFalse((new Argument('no'))->is(TypeCode::PHP_ARRAY | TypeCode::STRING & ~TypeCode::PHP_STRING));
+            $this->assertTrue((new Argument(true))->is(TypeCode::PHP_BOOL));
+            $this->assertFalse((new Argument(1))->is(TypeCode::PHP_BOOL));
+            $this->assertTrue((new Argument(function () {
+            }))->is(TypeCode::PHP_CALLABLE));
+            $this->assertTrue((new Argument([$this, 'testIsCallable']))->is(TypeCode::PHP_CALLABLE));
+            $this->assertTrue((new Argument('\PowerEcommerce\System\ArgumentTest::testIsCallable'))->is(TypeCode::STRING | TypeCode::PHP_CALLABLE));
+            $this->assertFalse((new Argument(1))->is(TypeCode::PHP_CALLABLE));
+            $this->assertTrue((new Argument(1.0))->is(TypeCode::PHP_DOUBLE));
+            $this->assertFalse((new Argument(1))->is(TypeCode::PHP_DOUBLE));
+            $this->assertTrue((new Argument(1.0))->is(TypeCode::PHP_FLOAT));
+            $this->assertFalse((new Argument(1))->is(TypeCode::PHP_FLOAT));
+            $this->assertTrue((new Argument(1))->is(TypeCode::PHP_INT));
+            $this->assertFalse((new Argument(1.0))->is(TypeCode::PHP_INT));
+            $this->assertTrue((new Argument(1))->is(TypeCode::PHP_INTEGER));
+            $this->assertFalse((new Argument(1.0))->is(TypeCode::PHP_INTEGER));
+            $this->assertTrue((new Argument(1))->is(TypeCode::PHP_INTEGER));
+            $this->assertFalse((new Argument(1.0))->is(TypeCode::PHP_INTEGER));
+            $this->assertTrue((new Argument(null))->is(TypeCode::PHP_NULL));
+            $this->assertFalse((new Argument(0))->is(TypeCode::PHP_NULL));
+            $this->assertTrue((new Argument('123.56'))->is(TypeCode::PHP_NUMERIC));
+            $this->assertTrue((new Argument(123.56))->is(TypeCode::PHP_NUMERIC));
+            $this->assertTrue((new Argument(123))->is(TypeCode::PHP_NUMERIC));
+            $this->assertFalse((new Argument('123,12'))->is(TypeCode::PHP_NUMERIC));
+            $this->assertTrue((new Argument(new \StdClass()))->is(TypeCode::PHP_OBJECT));
+            $this->assertFalse((new Argument(null))->is(TypeCode::PHP_OBJECT));
+            $this->assertTrue((new Argument(1.2))->is(TypeCode::PHP_REAL));
+            $this->assertFalse((new Argument(1))->is(TypeCode::PHP_REAL));
+            $this->assertTrue((new Argument(tmpfile()))->is(TypeCode::PHP_RESOURCE));
+            $this->assertFalse((new Argument(null))->is(TypeCode::PHP_RESOURCE));
+            $this->assertTrue((new Argument(1))->is(TypeCode::PHP_SCALAR));
+            $this->assertTrue((new Argument(1.2))->is(TypeCode::PHP_SCALAR));
+            $this->assertTrue((new Argument('1.2'))->is(TypeCode::PHP_SCALAR));
+            $this->assertTrue((new Argument(true))->is(TypeCode::PHP_SCALAR));
+            $this->assertFalse((new Argument(null))->is(TypeCode::PHP_SCALAR));
+            $this->assertFalse((new Argument([]))->is(TypeCode::PHP_SCALAR));
+            $this->assertFalse((new Argument(new \StdClass()))->is(TypeCode::PHP_SCALAR));
+            $this->assertFalse((new Argument(tmpfile()))->is(TypeCode::PHP_SCALAR));
+            $this->assertTrue((new Argument('1.2'))->is(TypeCode::PHP_STRING));
+            $this->assertFalse((new Argument(null))->is(TypeCode::PHP_STRING));
+            $this->assertFalse((new Argument([]))->is(TypeCode::PHP_STRING));
+            $this->assertFalse((new Argument(new \StdClass()))->is(TypeCode::PHP_STRING));
+            $this->assertFalse((new Argument(tmpfile()))->is(TypeCode::PHP_STRING));
+            $this->assertFalse((new Argument(1))->is(TypeCode::PHP_STRING));
+            $this->assertFalse((new Argument(1.2))->is(TypeCode::PHP_STRING));
+        }
+
+        function testOfBlank()
+        {
+            $this->assertTrue((new Argument(new Blank()))->ofBlank());
+            $this->assertFalse((new Argument(null))->ofBlank());
+        }
+
+        function testOfContainer()
+        {
+            $this->assertTrue((new Argument(new Container()))->ofContainer());
+            $this->assertFalse((new Argument(null))->ofContainer());
+        }
+
+        function testOfDateTime()
+        {
+            $this->assertTrue((new Argument(new DateTime()))->ofDateTime());
+            $this->assertFalse((new Argument(null))->ofDateTime());
+        }
+
+        function testOfNumber()
+        {
+            $this->assertTrue((new Argument(new Number()))->ofNumber());
+            $this->assertFalse((new Argument(null))->ofNumber());
+        }
+
+        function testOfObject()
+        {
+            $this->assertTrue((new Argument(new Number()))->ofObject());
+            $this->assertFalse((new Argument(null))->ofObject());
+        }
+
+        function testOfString()
+        {
+            $this->assertTrue((new Argument(new String()))->ofString());
+            $this->assertFalse((new Argument(null))->ofString());
+        }
+
+        function testOfTimeZone()
+        {
+            $this->assertTrue((new Argument(new TimeZone()))->ofTimeZone());
+            $this->assertFalse((new Argument(null))->ofTimeZone());
+        }
+
+        function testOf()
+        {
+            $this->assertTrue((new Argument(new Blank()))->of(TypeCode::BLANK));
+            $this->assertFalse((new Argument(null))->of(TypeCode::BLANK));
+            $this->assertTrue((new Argument(new Container()))->of(TypeCode::BLANK | TypeCode::CONTAINER));
+            $this->assertFalse((new Argument(null))->of(TypeCode::CONTAINER));
+            $this->assertTrue((new Argument(new DateTime()))->of(TypeCode::DATETIME));
+            $this->assertFalse((new Argument(null))->of(TypeCode::DATETIME));
+            $this->assertTrue((new Argument(new Number()))->of(TypeCode::NUMBER));
+            $this->assertFalse((new Argument(null))->of(TypeCode::NUMBER));
+            $this->assertTrue((new Argument(new String()))->of(TypeCode::STRING));
+            $this->assertFalse((new Argument(null))->of(TypeCode::STRING));
+            $this->assertTrue((new Argument(new TimeZone()))->of(TypeCode::TIMEZONE));
+            $this->assertFalse((new Argument(null))->of(TypeCode::TIMEZONE));
+        }
+
+        /**
+         * @expectedException \InvalidArgumentException
+         */
+        function testInvalid()
+        {
+            (new Argument('Test'))->invalid();
+        }
+    }
+}
